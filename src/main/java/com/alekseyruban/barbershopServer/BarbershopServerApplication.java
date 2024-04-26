5e9b6402c89e72721bb8095cac9d45d2593d8fb5
aleksey-ruban <alekseyruban555@gmail.com>
2024-04-26 16:49:46 +0300

    first init

---
+++ src/main/java/com/alekseyruban/barbershopServer/BarbershopServerApplication.java
@@ -0,0 +1,13 @@
+package com.alekseyruban.barbershopServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alekseyruban/barbershopServer/BarbershopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekseyruban.barbershopServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
