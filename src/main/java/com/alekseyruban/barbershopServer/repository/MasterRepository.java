86db47b7e754b1f4b5df7b8b48d33e0c8bae52b6
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-01 21:28:28 +0300

    Fix repositories issures

--- src/main/java/com/alekseyruban/barbershopServer/repository/MasterRepository.java
+++ src/main/java/com/alekseyruban/barbershopServer/repository/MasterRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaterRepository extends JpaRepository<Master, Long> {
+public interface MasterRepository extends JpaRepository<Master, Long> {
 
 }
