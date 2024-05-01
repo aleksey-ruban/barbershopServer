fd9370cb0d0e09a931b51eeb501775e0b0dca46b
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-01 21:28:28 +0300

    Fix repositories issures

--- src/main/java/com/alekseyruban/barbershopServer/repository/RecordRepository.java
+++ src/main/java/com/alekseyruban/barbershopServer/repository/RecordRepository.java
@@ -1,6 +1,7 @@
 package com.alekseyruban.barbershopServer.repository;
 
 import com.alekseyruban.barbershopServer.entity.RecordKey;
+import com.alekseyruban.barbershopServer.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
