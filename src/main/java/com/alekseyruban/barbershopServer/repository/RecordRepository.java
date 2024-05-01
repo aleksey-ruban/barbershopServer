5a55e4cfb8386b0f7436a4178286e029ec603c66
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-01 20:50:30 +0300

    Repositories created

---
+++ src/main/java/com/alekseyruban/barbershopServer/repository/RecordRepository.java
@@ -0,0 +1,10 @@
+package com.alekseyruban.barbershopServer.repository;
+
+import com.alekseyruban.barbershopServer.entity.RecordKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, RecordKey> {
+
+}
