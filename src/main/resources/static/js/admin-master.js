f322bca071a16af83ac2fad306e8704f499f7634
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-04 00:56:39 +0300

    Admin scripts for masters and services

---
+++ src/main/resources/static/js/admin-master.js
@@ -0,0 +1,29 @@
+"use strict";
+
+document.getElementById("postForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let formData = new FormData(document.getElementById("postForm"));
+    fetch("/admin/masters/", {
+        method: "POST",
+        body: formData
+    }).then(response => {
+        console.log("POST запрос выполнен успешно");
+    }).catch(error => {
+        console.error('Ошибка при отправке запроса:', error);
+    });
+        event.target.reset();
+});
+
+document.getElementById("deleteForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let delData = new FormData(document.getElementById("deleteForm"));
+    fetch("/admin/masters/", {
+        method: "DELETE",
+        body: delData
+    }).then(response => {
+        console.log("DELETE запрос выполнен успешно");
+    }).catch(error => {
+        console.error('Ошибка при отправке запроса:', error);
+    });
+        event.target.reset();
+});
