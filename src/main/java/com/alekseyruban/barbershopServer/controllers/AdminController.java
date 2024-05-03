1339034fde2fb6e80b01d5b071b39e189d21dba4
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-04 00:34:27 +0300

    AdminController improved
    Added services control

--- src/main/java/com/alekseyruban/barbershopServer/controllers/AdminController.java
+++ src/main/java/com/alekseyruban/barbershopServer/controllers/AdminController.java
@@ -1,10 +1,12 @@
 package com.alekseyruban.barbershopServer.controllers;
 
 import com.alekseyruban.barbershopServer.dto.MasterDTO;
+import com.alekseyruban.barbershopServer.dto.TreatmentServiceDTO;
 import com.alekseyruban.barbershopServer.entity.Master;
+import com.alekseyruban.barbershopServer.entity.TreatmentService;
 import com.alekseyruban.barbershopServer.enums.MasterQualification;
 import com.alekseyruban.barbershopServer.service.MasterService;
-import jakarta.websocket.server.PathParam;
+import com.alekseyruban.barbershopServer.service.TreatmentServiceService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.lang.reflect.Method;
 
 @Controller
 @RequestMapping({"/admin/", "/admin"})
@@ -23,6 +24,7 @@ import java.lang.reflect.Method;
 public class AdminController {
 
     private final MasterService masterService;
+    private final TreatmentServiceService treatmentServiceService;
 
     @GetMapping({"", "/"})
     public String defaultPage () {
@@ -61,9 +63,36 @@ public class AdminController {
         masterService.delete(idMaster);
     }
 
-    @GetMapping({"/services", "/services/"})
-    public String services() {
+    @RequestMapping(value = {"/services", "/services/"}, method = RequestMethod.POST)
+    public ResponseEntity<TreatmentService> createService(@RequestParam String name, @RequestParam Integer coast,
+                                                          @RequestParam Integer duration) {
+        TreatmentServiceDTO treatmentServiceDTO = TreatmentServiceDTO.builder()
+                .name(name)
+                .coast(coast)
+                .duration(duration)
+                .build();
+
+        return new ResponseEntity<>(treatmentServiceService.create(treatmentServiceDTO), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = {"/services", "/services/"}, method = RequestMethod.GET)
+    public String getServiceId(@RequestParam(value = "name", required = false) String name, Model model) {
+        if (name == null) {
+            model.addAttribute("answer", "Здесь появится ID");
+        } else {
+            Long serviceId = treatmentServiceService.getId(name);
+            if (serviceId == null) {
+                model.addAttribute("answer", "Такой услуги нет");
+            } else {
+                model.addAttribute("answer", "ID услуги: " + serviceId);
+            }
+        }
         return "admin/services";
+    }
+
+    @RequestMapping(value = {"/services", "/services/"}, method = RequestMethod.DELETE)
+    public void deleteService(@RequestParam Long idService) {
+        treatmentServiceService.delete(idService);
     }
 
     @GetMapping({"/records", "/records/"})
