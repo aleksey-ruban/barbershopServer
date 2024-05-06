3a7ac0a6ae2b1bc00f8c159eb8b819671afe4a7d
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-07 00:34:32 +0300

    Added new home paths

--- src/main/java/com/alekseyruban/barbershopServer/controllers/HomeController.java
+++ src/main/java/com/alekseyruban/barbershopServer/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "home", "/home/"})
     public String index() {
         return "index";
     }
