72df3d2545a264e0683a910f4d4d5a761d7ea08e
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-07 00:25:58 +0300

    Repositories improvements for future

--- src/main/java/com/alekseyruban/barbershopServer/repository/AuthorizationTokenRepository.java
+++ src/main/java/com/alekseyruban/barbershopServer/repository/AuthorizationTokenRepository.java
@@ -1,10 +1,16 @@
 package com.alekseyruban.barbershopServer.repository;
 
 import com.alekseyruban.barbershopServer.entity.AuthorizationToken;
+import com.alekseyruban.barbershopServer.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorizationTokenRepository extends JpaRepository<AuthorizationToken, Long> {
     AuthorizationToken findByToken(String token);
+
+    @Query("SELECT t FROM AuthorizationToken t WHERE t.client.id = :clientid")
+    AuthorizationToken findByClientId(@Param("clientid") Long clientid);
 }
