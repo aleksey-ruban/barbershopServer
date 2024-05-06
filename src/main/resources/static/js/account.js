e9ee66a7b5511d206484a66146c12f4b90be613b
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2024-05-07 00:39:50 +0300

    Record Controller finished + frontend improvements

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,12 +1,33 @@
 "use strict";
 
-document.querySelector(".delete-record-button").addEventListener("click", function(event) {
-    var id = event.target.getAttribute("record-id");
-    fetch("/record/delete-record?record-id=" + id, {
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.querySelector(".delete-record-button");
+    if (button != null) {
+        button.addEventListener("click", function(event) {
+            var id = event.target.getAttribute("record-id");
+            fetch("/record/delete-record?record-id=" + id, {
+                method: "DELETE",
+            }).then(response => {
+                console.log("DELETE запрос выполнен успешно");
+                var url = "/authorization/account";
+                window.location.href = url;
+            }).catch(error => {
+                console.error('Ошибка при отправке запроса:', error);
+            });
+        });
+    }
+})
+
+document.querySelector(".account-logout").addEventListener("click", function(event) {
+    window.location.href = "/logout";
+});
+
+document.querySelector(".account-delete").addEventListener("click", function(event) {
+    fetch("/authorization/account", {
         method: "DELETE",
     }).then(response => {
         console.log("DELETE запрос выполнен успешно");
-        var url = "/authorization/account";
+        var url = "/logout";
         window.location.href = url;
     }).catch(error => {
         console.error('Ошибка при отправке запроса:', error);
